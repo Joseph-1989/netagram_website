5f908983841de79e5ca6bd94908502f46a8a42f1
Joseph-1989 <akmal.khasanov1989@gmail.com>
2026-02-27 00:21:46 +0900

    fix: added english version to contents component

--- src/components/ContentsSection.tsx
+++ src/components/ContentsSection.tsx
@@ -1,8 +1,17 @@
+'use client';
+
+import { useLanguage } from '@/hooks/useLanguage';
+
 export default function ContentsSection() {
+  const language = useLanguage();
   return (
     <section id="features" className="flex mt-10 bg-white h-[calc(100vh-80px)]">
       <div className="w-full flex justify-center items-center">
-        <p className="text-2xl font-bold">CONTENTS 페이지는 현재 개발 중입니다.</p>
+        <p className="text-2xl font-bold">
+          {language === 'ko'
+            ? 'CONTENTS 페이지는 현재 개발 중입니다.'
+            : 'CONTENTS page is currently under development.'}
+        </p>
       </div>
     </section>
   );
